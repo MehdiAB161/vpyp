b9e1bb7d193ddce8511493b2741c919c2e8a347f
MehdiAB161 <mehdi-b-@live.fr>
2018-01-14 19:18:00 +0100

    Update Particle Filter

--- vpyp/postag/train.py
+++ vpyp/postag/train.py
@@ -7,44 +7,44 @@ from prob import Uniform
 from pyp import PYP
 from prior import PYPPrior
 from model import PYPLM
+import numpy as np
 
 from new_functions import sample_multinomial
 
 # mh_iter = 100  # number of Metropolis-Hastings sampling iterations
 
 
-def run_sampler(model, corpus, n_iter, P):
+def run_sampler(model, corpus, n_iter, n_particles, n_tags):
     n_sentences = len(corpus)
     n_words = sum(len(sentence) for sentence in corpus)
+
+    all_tags = np.arange(n_tags)
+
     for it in range(n_iter):
         logging.info('Iteration %d/%d', it+1, n_iter)
         for sentence in corpus:
-            n = 0
-
-            # We decrement the transitions and emissions of the sentence from the table arrangements
             for seq in ngrams(sentence, 3):
-
                 if it > 0: model.decrement(seq[0:2], seq[2])
                 model.increment(seq[:-1], seq[-1])
 
-                # Remove the current sentence
+        for sentence in corpus:
+            # TODO : decrement emissions
+            for seq in ngrams(sentence, 3):
+                # We decrement the transitions and emissions of the sentence from the table arrangements
                 model.decrement(seq[:-1], seq[-1])
-
-                # TODO : decrement emissions
 
             for seq in ngrams(sentence, 5):
 
                 # We compute all the possible sequences by replacing the middle tag
-                possible_sequences = [seq[0:2] + [w] + seq[4:6] for w in all_tags]
+                possible_sequences = [seq[0:2] + [w] + seq[3:5] for w in all_tags]
 
                 # We compute the proposal distribution
                 proposal_distribution = [model.prob(ctx=seq[0:2], w=seq[2]) * model.prob(ctx=seq[1:3], w=seq[3])
                                          * model.prob(ctx=seq[2:4], w=seq[5]) for seq in possible_sequences]
 
-
                 # Pick P particles from the proposal distribution
 
-                particles = sample_multinomial(P, proposal_distribution)
+                particles = sample_multinomial(n_particles, proposal_distribution)
 
                 # Update the seating arrangements for all the particles
                 for particle in particles :
@@ -60,7 +60,7 @@ def run_sampler(model, corpus, n_iter, P):
                 weights = [weights[i] * alphas[i] for i in range(len(weights))]
 
                 # Resample the particles to obtain P equally weighted particles
-                particles = sample_multinomial(P, weights)
+                particles = sample_multinomial(n_particles, weights)
 
         if it % 10 == 0:
             logging.info('Model: %s', model)
@@ -87,6 +87,7 @@ def main():
     parser.add_argument('--iter', help='number of iterations', type=int, required=True)
     parser.add_argument('--output', help='model output path')
     parser.add_argument('--P', help='Number of particles')
+    parser.add_argument('--T', help='Number of tags')
 
     args = parser.parse_args()
 
@@ -108,7 +109,7 @@ def main():
     model = PYPLM(args.order, base)
 
     logging.info('Training model of order %d', args.order)
-    run_sampler(model, training_corpus, args.iter)
+    run_sampler(model, training_corpus, args.iter, n_particles=args.P, n_tags=args.T)
 
     if args.output:
         model.vocabulary = vocabulary
